db32d2ddffa7967b5ac9302e83c87fb5fde5eeb2
k99k5 <545551320@qq.com>
2024-02-04 00:34:44 +0800

    fix: local-database encode function error

--- src/plugins/local-database.js
+++ src/plugins/local-database.js
@@ -69,6 +69,9 @@ export async function localDatabase({store}) {
             }
             if (saveMode) {
                 for (const key of Object.keys(state)) {
+                    if (state[key] === undefined ||  typeof state[key] === 'function') {
+                        continue;
+                    }
                     instance.setItem(
                         key,
                         {
