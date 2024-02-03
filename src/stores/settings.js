842ece1cdbddaaf0d2c541d0e9c2dd4d31103726
k99k5 <545551320@qq.com>
2024-02-04 00:38:08 +0800

    feat: add cookie library

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -1,86 +1,11 @@
 import {defineStore} from 'pinia';
+import {useCookieManager} from "@/utils/cookie.js";
 
 export const useSettingStore = defineStore({
     id: 'settings',
     state: () => {
-        return {
-            guildVersion: 0,
-            chapter: {
-                bgColor: 'rgba(232,232,232,1)',
-                font: 'MiSans',
-                fontColor: '#000000',
-                fontSize: 20,
-                padding: 40,
-                autoPlay: false,
-                viewType: 'book',
-            },
-            global: {
-                themeMode: 'auto',
-                safeMode: true,
-                autoTranslate: false,
-                translateTo: 'auto',
-                acceptedLanguages: [
-                    {
-                        name: '中文',
-                        value: 'zh',
-                    },
-                    {
-                        name: '英语',
-                        value: 'en',
-                    },
-                    {
-                        name: '日語',
-                        value: 'ja',
-                    },
-                    {
-                        name: '韩语',
-                        value: 'ko',
-                    }
-                ],
-                hideLanguages: ['en', 'ja', 'ko'],
-                hideTags: [],
-            }
-        }
+        return {};
     },
-    actions: {},
-    getters: {
-        showLanguages(state) {
-            return state.global.acceptedLanguages.filter((lang) => {
-                return !state.global.hideLanguages.includes(lang.value);
-            });
-        },
-        computedHateTags(state) {
-            let tags = [];
-            let showLanguages = state.global.acceptedLanguages.filter((lang) => {
-                return !state.global.hideLanguages.includes(lang.value);
-            });
-            if (state.global.safeMode) {
-                tags.push('r-18');
-                tags.push('R18');
-            }
-            if (showLanguages.length > 1 || showLanguages.length === 0) {
-                if (state.global.hideLanguages.length > 1) {
-                    tags = tags.concat(state.global.hideLanguages.map((lang) => {
-                        return lang;
-                    }));
-                }
-            }
-            if (state.global.hideTags.length > 0) {
-                tags = tags.concat(state.global.hideTags);
-            }
-            return tags;
-        },
-        computedTags(state) {
-            let tags = [];
-            let showLanguages = state.global.acceptedLanguages.filter((lang) => {
-                return !state.global.hideLanguages.includes(lang.value);
-            });
-            if (showLanguages.length === 1) {
-                tags.push(showLanguages[0].value);
-            }
-            return tags;
-        },
-    }
 });
 export default {
     useSettingStore,
