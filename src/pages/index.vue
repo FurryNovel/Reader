673fe8dde291ae48a9826691efa1462cec5c61e1
k99k5 <545551320@qq.com>
2024-02-07 21:09:53 +0800

    feat: update index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,44 +1,40 @@
 <template>
 	<div class="flex h-full w-full flex-col">
-		<div class="flex p-10 flex-1 flex-col rounded bg-white dark:bg-surface-600 text-black dark:text-white">
-			<div class="flex items-center justify-between m-2">
+		<div class="flex flex-1 flex-col rounded bg-white p-10 text-black dark:bg-surface-600 dark:text-white">
+			<div class="m-2 flex items-center justify-between">
 				<div class="text-2xl font-bold">热门精选</div>
 				<router-link :to="{name:'search'}">
-					<Button class="text-sm mr-2 text-primary-500" label="查看更多" size="small">
+					<Button class="mr-2 text-sm text-primary-500" label="查看更多" size="small">
 						查看更多
-						<div class="fa-regular fa-chevron-right ml-2"></div>
+						<div class="ml-2 fa-regular fa-chevron-right"></div>
 					</Button>
 				</router-link>
-			
 			</div>
-			<NovelList type="random" order="desc" listStyle="style1" :desc="false" :author="false" :tags="null"
-			           :limit="8" v-ssr
+			<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false" :tags="null"
+			           :limit="8"
 			           :userId="null" :keyword="null" :ids="null"/>
 		</div>
-		<div class="flex p-10 flex-1 flex-col rounded bg-white dark:bg-surface-600 text-black dark:text-white">
-			<div class="flex items-center justify-between m-2">
-				<div class="text-2xl font-bold">热门精选</div>
+		<div class="flex flex-1 flex-col rounded bg-white p-10 text-black dark:bg-surface-600 dark:text-white">
+			<div class="m-2 flex items-center justify-between">
+				<div class="text-2xl font-bold">最新小说</div>
 				<router-link :to="{name:'search'}">
-					<Button class="text-sm mr-2 text-primary-500" label="查看更多" size="small">
+					<Button class="mr-2 text-sm text-primary-500" label="查看更多" size="small">
 						查看更多
-						<div class="fa-regular fa-chevron-right ml-2"></div>
+						<div class="ml-2 fa-regular fa-chevron-right"></div>
 					</Button>
 				</router-link>
-			
 			</div>
-			<NovelList type="random" order="desc" listStyle="style1" :desc="false" :author="false" :tags="null"
-			           :limit="8" v-ssr
+			<NovelList v-ssr type="random" order="desc" listStyle="style1" :desc="false" :author="false" :tags="null"
+			           :limit="8"
 			           :userId="null" :keyword="null" :ids="null"/>
 		</div>
 	</div>
 </template>
 
 <script setup>
-import dialog from "@/utils/dialog.js";
-import NovelList from "@/components/views/NovelList.vue";
+import dialog from '@/utils/dialog.js';
+import NovelList from '@/components/views/NovelList.vue';
 
 onServerPrefetch(() => {
-
 });
-
 </script>
