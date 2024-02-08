fb5b1eaa96b5705782a724588215eb95125cb135
k99k5 <k99k5@qq.com>
2024-02-08 10:41:47 +0800

    feat: update search style

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -3,8 +3,15 @@
 		<div class="flex flex-1 flex-col rounded bg-white p-10 text-black dark:bg-surface-600 dark:text-white">
 			<template v-if="data.keyword !== ''">
 				<div class="m-2 flex items-center justify-between">
-					<div class="text-2xl font-bold">搜索</div>
-					<router-link :to="{name:'search'}" :draggable="false">
+					<div class="text-2xl font-bold">
+						<Button class="text-sm text-primary-500" label="条件筛选" outlined
+						        severity="secondary" size="small"
+						        @click="data.keyword = ''">
+							<div class="fa-regular fa-chevron-left"></div>
+						</Button>
+						搜索
+					</div>
+					<router-link :draggable="false" :to="{name:'search'}">
 						<Button class="mr-2 text-sm text-primary-500" label="条件筛选"
 						        size="small">
 							条件筛选
@@ -12,20 +19,20 @@
 						</Button>
 					</router-link>
 				</div>
-				<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false"
-				           :author="false" :tags="null"
-				           :limit="8"
-				           :userId="null" :keyword="data.keyword" :ids="null"/>
+				<NovelList v-ssr :author="false" :desc="false" :ids="null" :keyword="data.keyword"
+				           :limit="8" :tags="null"
+				           :userId="null"
+				           listStyle="style2" order="desc" type="popular"/>
 			</template>
 			<template v-else>
 				<div class="flex flex-col items-center justify-center min-h-[calc(100vh-208px)] w-full">
-					<InputGroup class="flex flex-row w-full justify-center items-center group duration-200">
+					<InputGroup class="flex w-full flex-row items-center justify-center duration-200 group">
 						<InputGroupAddon class="bg-white !border-r-0 group-hover:border-primary-500">
 							<i class="fa-regular fa-magnifying-glass text-surface-400 dark:text-surface-600"></i>
 						</InputGroupAddon>
 						<InputText id="keyword" v-model="data.preKeyword"
-						           placeholder="请输入关键字：小说名、作者名、简介等"
-						           class="w-5/12 max-sm:w-max !border-l-0 focus:ring-0 hover:border-surface-300 group-hover:border-primary-500 !duration-0"/>
+						           class="w-5/12 max-sm:w-max !border-l-0 focus:ring-0 hover:border-surface-300 group-hover:border-primary-500 !duration-0"
+						           placeholder="请输入关键字：小说名、作者名、简介等"/>
 						<Button label="搜索" @click="data.keyword = data.preKeyword"/>
 					</InputGroup>
 				</div>
