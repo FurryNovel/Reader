c2d756df39d555de70662f95b6616a5e00549787
k99k5 <545551320@qq.com>
2024-02-11 15:10:35 +0800

    🔧 refactor: Move keywords and description to config.js

--- src/utils/meta.js
+++ src/utils/meta.js
@@ -14,8 +14,8 @@ const data = reactive({
 const computeParams = computed(() => {
     return {
         title: config.title,
-        keywords: '兽人控,兽人小说,兽人漫画,兽人同人,兽人文学',
-        description: '兽人控小说站是一个专为兽人控打造的聚合在线阅读平台。在这里，你可以找到各种平台各种兽人相关的小说，从兽人与人类的爱恨情仇，到兽人之间的种族斗争，从兽人的历史文化，到兽人的奇幻冒险，应有尽有。你可以随时随地享受阅读的乐趣，无需下载或安装任何软件。还可以自由切换翻译，选择你喜欢的阅读模式。',
+        keywords: config.keywords,
+        description: config.description,
         image: iconUrl,
         ...data.route?.meta,
     };
@@ -25,9 +25,15 @@ export function initRouter(router) {
     data.route = router.currentRoute.value;
     router.afterEach((to) => {
         data.route = to;
+        data.title = computeParams.value.title;
+        data.keywords = computeParams.value.keywords;
+        data.description = computeParams.value.description;
+        data.image = computeParams.value.image ? computeParams.value.image : iconUrl;
+        data.url = computeParams.value.url ? computeParams.value.url : `${config.base}${data.route?.path || ''}`;
     });
     useHead({
         title: computed(() => {
+            console.log(data.title)
             return data.title;
         }),
         meta: computed(() => {
@@ -59,13 +65,6 @@ export function initRouter(router) {
             ];
         }),
     });
-    watchEffect(() => {
-        data.title = computeParams.value.title;
-        data.keywords = computeParams.value.keywords;
-        data.description = computeParams.value.description;
-        data.image = computeParams.value.image ? computeParams.value.image : iconUrl;
-        data.url = computeParams.value.url ? computeParams.value.url : `${config.base}${data.route?.path || ''}`;
-    });
 }
 
 export function useMeta(params) {
@@ -77,9 +76,9 @@ export function useMeta(params) {
             useMeta(params.value);
         });
     } else {
-        data.title = params.title;
-        data.keywords = params.keywords;
-        data.description = params.description;
+        data.title = params.title ? params.title : config.title;
+        data.keywords = params.keywords ? params.keywords : config.keywords;
+        data.description = params.description ? params.description : config.description;
         data.image = params.image ? params.image : iconUrl;
         data.url = params.url ? params.url : `${config.base}${data.route?.path || ''}`;
     }
