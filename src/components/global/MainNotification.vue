3a38b96fdca6c1080264f30fd86f754c21620968
k99k5 <545551320@qq.com>
2024-02-13 21:02:42 +0800

    🚀 feat: Add input dialog

--- src/components/global/MainNotification.vue
+++ src/components/global/MainNotification.vue
@@ -3,6 +3,10 @@
 		<span class="p-text-secondary block mb-5 select-none">
 			{{ data.dialog.content }}
 		</span>
+		<div v-if="data.dialog.input !== null" class=" mb-5 ">
+			<InputText v-model="data.dialog.input" size="small" class="text-sm w-full"
+			           :placeholder="data.dialog.input"/>
+		</div>
 		<div class="flex justify-end gap-2">
 			<Button v-if="data.dialog.cancelBtn" :label="data.dialog.cancelBtn" severity="secondary" type="button"
 			        @click="clickBtn('cancel')"></Button>
@@ -10,7 +14,7 @@
 			        @click="clickBtn('confirm')"></Button>
 		</div>
 	</Dialog>
-	<Toast />
+	<Toast/>
 </template>
 <script setup>
 import eventbus from "@/utils/eventbus.js";
@@ -25,29 +29,35 @@ const data = reactive({
         content: '内容',
         cancelBtn: null,
         confirmBtn: '确定',
+        input: null,
     }
 });
 
-eventbus.on('showDialog', ({title, content, cancelBtn, confirmBtn}) => {
-	data.dialog.title = title;
-	data.dialog.content = content;
-	data.dialog.cancelBtn = cancelBtn;
-	data.dialog.confirmBtn = confirmBtn;
-	data.dialog.show = true;
+eventbus.on('showDialog', ({title, content, cancelBtn, confirmBtn, input = null}) => {
+    data.dialog.title = title;
+    data.dialog.content = content;
+    data.dialog.cancelBtn = cancelBtn;
+    data.dialog.confirmBtn = confirmBtn;
+    data.dialog.input = input;
+    console.log(input)
+    data.dialog.show = true;
 });
 
 
 eventbus.on('showToast', ({title, content, type}) => {
     toast.add({
-        severity:type,
+        severity: type,
         summary: title,
         detail: content,
     });
 });
 
-function clickBtn(type){
+function clickBtn(type) {
     data.dialog.show = false;
-    eventbus.emit('dialogClick', type);
+    eventbus.emit('dialogClick', {
+        type,
+        ...data.dialog,
+    });
 }
 </script>
 
--- src/utils/dialog.js
+++ src/utils/dialog.js
@@ -3,14 +3,27 @@ import eventbus from "@/utils/eventbus.js";
 export default {
     alert({title, content, cancelBtn, confirmBtn}) {
         return new Promise((resolve, reject) => {
-            eventbus.once('dialogClick', type => {
+            eventbus.once('dialogClick', ({type}) => {
                 if (type === 'confirm') {
                     resolve();
-                }else{
+                } else {
                     reject();
                 }
             });
             eventbus.emit('showDialog', {title, content, cancelBtn, confirmBtn});
+        });
+    },
+    input({title, content, cancelBtn, confirmBtn, placeholder = ''}) {
+        return new Promise((resolve, reject) => {
+            eventbus.once('dialogClick', data => {
+                const {type} = data;
+                if (type === 'confirm') {
+                    resolve(data);
+                } else {
+                    reject(data);
+                }
+            });
+            eventbus.emit('showDialog', {title, content, cancelBtn, confirmBtn, input: placeholder});
         });
     },
     toast({type, content, title = '提示'}) {
@@ -23,6 +36,6 @@ export default {
                 resolve(e);
             });
         });
-      
+        
     }
 }
