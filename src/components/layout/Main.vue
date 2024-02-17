ebeec9e975702b5905c1c3cbbcdba962e5bb9b2b
k99k5 <545551320@qq.com>
2024-02-17 22:41:56 +0800

    🚚 refactor: change style function in Main.vue

--- src/components/layout/Main.vue
+++ src/components/layout/Main.vue
@@ -23,13 +23,18 @@ const router = useRouter();
 const wrapperStyle = ref(null);
 
 router.beforeEach((to, from, next) => {
+    changeStyle(to)
+    next();
+});
+onRouteChange(to => changeStyle(to));
+
+function changeStyle(to) {
     if (to.meta.style?.navBar !== undefined) {
         wrapperStyle.value = to.meta.style.navBar;
     } else {
         wrapperStyle.value = 'h-full w-full max-w-screen-xl flex flex-col mx-auto rounded-md bg-surface-50 dark:bg-surface-700 sm:border border-surface-200 dark:border-surface-700';
     }
-    next();
-});
+}
 
 </script>
 
