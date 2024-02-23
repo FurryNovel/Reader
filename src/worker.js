1fc16a3be64a926dc07deb047aed85d680d3895a
k99k5 <545551320@qq.com>
2024-02-24 02:29:35 +0800

    🔧 refactor: move try block to the top in src/worker.js

--- src/worker.js
+++ src/worker.js
@@ -4,25 +4,25 @@ import manifest from '@/../dist/client/.vite/ssr-manifest.json?raw';
 
 export default {
     async fetch(request, env) {
-        const url = new URL(request.url);
-        if (
-            url.pathname.startsWith('/client')
-            || url.pathname.startsWith('/server')
-        ) {
-            return await env.ASSETS.fetch(`http://hostname.invalid${url.pathname}`);
-        }
-        if (url.pathname.startsWith('/api/')) {
-            url.href = url.href.replace(url.hostname, env.API_HOST);
-            url.hostname = env.API_HOST;
-            return fetch(new Request(url, request));
-        } else if (
-            url.pathname.startsWith('/assets/')
-            || url.pathname.startsWith('/static/')
-            || url.pathname.indexOf('.') !== -1
-        ) {
-            return await env.ASSETS.fetch('http://hostname.invalid/client' + url.pathname);
-        }
         try {
+            const url = new URL(request.url);
+            if (
+                url.pathname.startsWith('/client')
+                || url.pathname.startsWith('/server')
+            ) {
+                return await env.ASSETS.fetch(`http://hostname.invalid${url.pathname}`);
+            }
+            if (url.pathname.startsWith('/api/')) {
+                url.href = url.href.replace(url.hostname, env.API_HOST);
+                url.hostname = env.API_HOST;
+                return fetch(new Request(url, request));
+            } else if (
+                url.pathname.startsWith('/assets/')
+                || url.pathname.startsWith('/static/')
+                || url.pathname.indexOf('.') !== -1
+            ) {
+                return await env.ASSETS.fetch('http://hostname.invalid/client' + url.pathname);
+            }
             const renderRes = await render(url.pathname, manifest, {
                 cookies: request.headers.get('cookie'),
             });
