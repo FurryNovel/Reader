762cf8b21467251b4bc698891e6be3def8061273
k99k5 <k99k5@qq.com>
2024-02-23 15:26:07 +0800

    🔧 refactor: Use ES modules for cloudflare-worker.js

--- build/cloudflare-worker.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-fs.writeFile(
-    path.resolve(__dirname, '../dist/_worker.js'),
-    fs.readFileSync(path.resolve(__dirname, '_worker.js'), 'utf-8'),
-    'utf-8',
-    (err) => {
-    }
-)
