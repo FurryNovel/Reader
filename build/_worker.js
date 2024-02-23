9fccb9f5768035c800b75b96a852027ffdc268ea
k99k5 <545551320@qq.com>
2024-02-23 20:37:17 +0800

    🐛 fix: handle client and server assets in _worker.js

--- build/_worker.js
+++ build/_worker.js
@@ -1,12 +1,11 @@
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
-        console.log(url, url.pathname)
         if (
             url.pathname.startsWith('/client')
             || url.pathname.startsWith('/server')
         ) {
-            return env.ASSETS.fetch(request);
+            return await env.ASSETS.fetch(url.pathname);
         }
         if (url.pathname.startsWith('/api/')) {
             url.href = url.href.replace(url.hostname, env.API_HOST);
@@ -17,8 +16,8 @@ export default {
             || url.pathname.startsWith('/static/')
             || url.pathname.indexOf('.') !== -1
         ) {
-            return env.ASSETS.fetch(new URL(request.url.replace(request.pathname, '/client' + url.pathname)));
+            return await env.ASSETS.fetch('/client' + url.pathname);
         }
-        return env.ASSETS.fetch(new URL(request.url.replace(request.pathname, '/client/index.html')));
+        return await env.ASSETS.fetch('/client/index.html');
     },
 }
