a02c888a2cdef7379d5d74e2887b684c6b689dd5
k99k5 <k99k5@qq.com>
2024-02-23 18:00:43 +0800

    :bug: fix:  replace hostname and pathname for API and client requests

--- build/_worker.js
+++ build/_worker.js
@@ -1,18 +1,20 @@
-
 export default {
     async fetch(request, env) {
         const url = new URL(request.url);
         if (url.pathname.startsWith('/api/')) {
+            url.href = url.replace(url.hostname, env.API_HOST);
             url.hostname = env.API_HOST;
             return await fetch(new Request(url, request));
-        }else if (
+        } else if (
             url.pathname.startsWith('/assets/')
             || url.pathname.startsWith('/static/')
             || url.pathname.indexOf('.') !== -1
         ) {
+            url.href = url.replace(url.pathname, '/client' + url.pathname);
             url.pathname = '/client' + url.pathname;
             return env.ASSETS.fetch(new Request(url, request));
         }
+        url.href = url.replace(url.pathname, '/client/index.html');
         url.pathname = '/client/index.html';
         return env.ASSETS.fetch(new Request(url, request));
     },
