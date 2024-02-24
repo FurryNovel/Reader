3dfe2ab75b87598c215dcede586288caa5a99d4a
k99k5 <545551320@qq.com>
2024-02-24 15:50:33 +0800

    todo: test worker route

--- worker.js
+++ worker.js
@@ -3,6 +3,24 @@ import path from 'node:path';
 
 const worker = fs.readFileSync(path.resolve('./cloudflare/functions/[[catchall]].js'), 'utf-8');
 
-const toPath = path.resolve('./dist/functions/[[catchall]].js');
-fs.mkdirSync(path.dirname(toPath), {recursive: true});
-fs.writeFileSync(toPath, worker);
+const basePath = path.resolve('./dist/functions/');
+
+const routes = [
+    'index',
+    'bookmark',
+    'about',
+    'search',
+    'settings',
+    'novel/*',
+];
+
+routes.forEach((route) => {
+    if (route.endsWith('*')) {
+        route = route.slice(0, -1);
+        route = route + '[[catchall]]';
+    }
+    const toPath = path.resolve(basePath, `${route}.js`);
+    fs.mkdirSync(path.dirname(toPath), {recursive: true});
+    fs.writeFileSync(toPath, worker);
+    console.log(`Created function for route: ${route}.js`);
+});
