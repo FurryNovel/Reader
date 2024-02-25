2d37ad5073916f7c324eeb169d4d470cc78f63fe
k99k5 <545551320@qq.com>
2024-02-26 00:29:42 +0800

    🎨 refactor: Change the text and list style of the index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,7 +17,7 @@
 		
 		<div class="flex flex-1 flex-col rounded bg-white p-10 text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
 			<div class="m-2 flex items-center justify-between">
-				<div class="text-2xl font-bold">按种族</div>
+				<div class="text-2xl font-bold">种族</div>
 			</div>
 			<div class="">
 				<div class="flex flex-1 flex-wrap max-sm:flex-col">
@@ -33,9 +33,9 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false"
+						<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false" :author="false"
 						           :tags="['虎']"
-						           :limit="4"
+						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
 					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
@@ -50,13 +50,11 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false"
+						<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false" :author="false"
 						           :tags="['龙']"
-						           :limit="4"
+						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
-				</div>
-				<div class="flex flex-1 flex-wrap max-sm:flex-col">
 					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">狼</div>
@@ -69,11 +67,13 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false"
+						<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false" :author="false"
 						           :tags="['狼']"
-						           :limit="4"
+						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
+				</div>
+				<div class="flex flex-1 flex-wrap max-sm:flex-col">
 					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">狗</div>
@@ -86,9 +86,43 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false"
+						<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false" :author="false"
 						           :tags="['狗']"
-						           :limit="4"
+						           :limit="3"
+						           :userId="null" :keyword="null" :ids="null"/>
+					</div>
+					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+						<div class="m-2 flex items-center justify-between">
+							<div class="text-xl font-bold">猫</div>
+							<router-link :to="{name:'list',query:{type:'popular',tags:'猫'}}" :draggable="false">
+								<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="查看更多"
+								        size="small"
+								        text>
+									查看更多
+									<div class="ml-2 fa-regular fa-chevron-right"></div>
+								</Button>
+							</router-link>
+						</div>
+						<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false" :author="false"
+						           :tags="['猫']"
+						           :limit="3"
+						           :userId="null" :keyword="null" :ids="null"/>
+					</div>
+					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+						<div class="m-2 flex items-center justify-between">
+							<div class="text-xl font-bold">狐</div>
+							<router-link :to="{name:'list',query:{type:'popular',tags:'狐'}}" :draggable="false">
+								<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="查看更多"
+								        size="small"
+								        text>
+									查看更多
+									<div class="ml-2 fa-regular fa-chevron-right"></div>
+								</Button>
+							</router-link>
+						</div>
+						<NovelList v-ssr type="popular" order="desc" listStyle="style2" :desc="false" :author="false"
+						           :tags="['狐']"
+						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
 				</div>
