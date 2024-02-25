a68ecec239b5a5893ec311923782a2717b82ee27
k99k5 <545551320@qq.com>
2024-02-26 00:46:38 +0800

    :sparkles: feat: Add random novel list to index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,40 @@
 <template>
 	<div class="flex h-full w-full flex-col">
 		<div class="flex flex-1 flex-col rounded bg-white p-10 text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
-			<div class="m-2 flex items-center justify-between">
-				<div class="text-2xl font-bold">热门精选</div>
-				<router-link :to="{name:'list'}" :draggable="false">
-					<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="查看更多" size="small" text>
-						查看更多
-						<div class="ml-2 fa-regular fa-chevron-right"></div>
-					</Button>
-				</router-link>
+			<div class="flex flex-1 flex-wrap max-sm:flex-col">
+				<div class="flex flex-[2] flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="m-2 flex items-center justify-between">
+						<div class="text-2xl font-bold">热门精选</div>
+						<router-link :to="{name:'list'}" :draggable="false">
+							<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="查看更多" size="small"
+							        text>
+								查看更多
+								<div class="ml-2 fa-regular fa-chevron-right"></div>
+							</Button>
+						</router-link>
+					</div>
+					<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false"
+					           :tags="null"
+					           :limit="15"
+					           :userId="null" :keyword="null" :ids="null"/>
+				</div>
+				<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="m-2 flex items-center justify-between">
+						<div class="text-2xl font-bold">随便看看</div>
+						<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="刷新一下" size="small"
+						        text @click="refreshRandomList">
+							刷新一下
+							<div class="ml-2 fa-regular fa-rotate-right"></div>
+						</Button>
+					</div>
+					<NovelList ref="randomList" v-ssr type="random" order="desc" listStyle="style2" :desc="false"
+					           :author="false"
+					           :tags="null"
+					           :limit="4"
+					           :userId="null" :keyword="null" :ids="null"/>
+				</div>
 			</div>
-			<NovelList v-ssr type="popular" order="desc" listStyle="style1" :desc="false" :author="false" :tags="null"
-			           :limit="16"
-			           :userId="null" :keyword="null" :ids="null"/>
 		</div>
-		
 		<div class="flex flex-1 flex-col rounded bg-white p-10 text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
 			<div class="m-2 flex items-center justify-between">
 				<div class="text-2xl font-bold">种族</div>
@@ -149,6 +169,12 @@
 <script setup>
 import dialog from '@/utils/dialog.js';
 import NovelList from '@/components/views/NovelList.vue';
+
+const randomList = ref(null);
+
+function refreshRandomList() {
+    randomList.value.reload();
+}
 
 onServerPrefetch(() => {
 });
