0271aef21a6c085353a32827a20ddd50daf5ca2b
k99k5 <k99k5@qq.com>
2024-03-04 13:54:00 +0800

    🚀 feat: add scroll behavior to router.js

--- src/router.js
+++ src/router.js
@@ -158,5 +158,21 @@ export function createRouter() {
     return _createRouter({
         history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
         routes: items,
+        scrollBehavior(to, from, savedPosition) {
+            if (savedPosition) {
+                return savedPosition
+            } else {
+                if (to.hash) {
+                    return {
+                        el: to.hash,
+                        behavior: 'smooth',
+                    }
+                }
+                return {
+                    top: 0,
+                    behavior: 'smooth',
+                }
+            }
+        },
     });
 }
