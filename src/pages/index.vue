54df49c4e3217351e00c6e962a95876a28fbc31f
k99k5 <k99k5@qq.com>
2024-03-11 14:48:09 +0800

    🎨 style: Change novel list type to random and adjust min-width
    - Change novel list type from popular to random for each category
    - Add sm:min-w-[399.33px] class to each novel list container
    - Swap the categories of 狗 and 熊
    - Swap the categories of 猫 and 狐

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -56,7 +56,7 @@
 			</div>
 			<div class="">
 				<div class="flex flex-1 flex-wrap max-sm:flex-col">
-					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="flex flex-1 flex-col rounded bg-white text-black sm:min-w-[399.33px] max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">虎</div>
 							<router-link :to="{name:'list',query:{type:'popular',tags:'虎'}}" :draggable="false">
@@ -68,12 +68,12 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style2"
+						<NovelList v-ssr type="random" order="desc" listStyle="style2"
 						           :tags="['虎']"
 						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
-					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="flex flex-1 flex-col rounded bg-white text-black sm:min-w-[399.33px] max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">龙</div>
 							<router-link :to="{name:'list',query:{type:'popular',tags:'龙'}}" :draggable="false">
@@ -85,12 +85,12 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style2"
+						<NovelList v-ssr type="random" order="desc" listStyle="style2"
 						           :tags="['龙']"
 						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
-					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="flex flex-1 flex-col rounded bg-white text-black sm:min-w-[399.33px] max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">狼</div>
 							<router-link :to="{name:'list',query:{type:'popular',tags:'狼'}}" :draggable="false">
@@ -102,14 +102,29 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style2"
+						<NovelList v-ssr type="random" order="desc" listStyle="style2"
 						           :tags="['狼']"
 						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
-				</div>
-				<div class="flex flex-1 flex-wrap max-sm:flex-col">
-					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="flex flex-1 flex-col rounded bg-white text-black sm:min-w-[399.33px] max-sm:p-3 dark:bg-surface-600 dark:text-white">
+						<div class="m-2 flex items-center justify-between">
+							<div class="text-xl font-bold">熊</div>
+							<router-link :to="{name:'list',query:{type:'popular',tags:'狐'}}" :draggable="false">
+								<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="查看更多"
+								        size="small"
+								        text>
+									查看更多
+									<div class="ml-2 fa-regular fa-chevron-right"></div>
+								</Button>
+							</router-link>
+						</div>
+						<NovelList v-ssr type="random" order="desc" listStyle="style2"
+						           :tags="['熊']"
+						           :limit="3"
+						           :userId="null" :keyword="null" :ids="null"/>
+					</div>
+					<div class="flex flex-1 flex-col rounded bg-white text-black sm:min-w-[399.33px] max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">狗</div>
 							<router-link :to="{name:'list',query:{type:'popular',tags:'狗'}}" :draggable="false">
@@ -121,12 +136,12 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style2"
+						<NovelList v-ssr type="random" order="desc" listStyle="style2"
 						           :tags="['狗']"
 						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
-					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
+					<div class="flex flex-1 flex-col rounded bg-white text-black sm:min-w-[399.33px] max-sm:p-3 dark:bg-surface-600 dark:text-white">
 						<div class="m-2 flex items-center justify-between">
 							<div class="text-xl font-bold">猫</div>
 							<router-link :to="{name:'list',query:{type:'popular',tags:'猫'}}" :draggable="false">
@@ -138,25 +153,8 @@
 								</Button>
 							</router-link>
 						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style2"
+						<NovelList v-ssr type="random" order="desc" listStyle="style2"
 						           :tags="['猫']"
-						           :limit="3"
-						           :userId="null" :keyword="null" :ids="null"/>
-					</div>
-					<div class="flex flex-1 flex-col rounded bg-white text-black max-sm:p-3 dark:bg-surface-600 dark:text-white">
-						<div class="m-2 flex items-center justify-between">
-							<div class="text-xl font-bold">狐</div>
-							<router-link :to="{name:'list',query:{type:'popular',tags:'狐'}}" :draggable="false">
-								<Button class="mr-2 text-sm text-primary-500 dark:text-white" label="查看更多"
-								        size="small"
-								        text>
-									查看更多
-									<div class="ml-2 fa-regular fa-chevron-right"></div>
-								</Button>
-							</router-link>
-						</div>
-						<NovelList v-ssr type="popular" order="desc" listStyle="style2"
-						           :tags="['狐']"
 						           :limit="3"
 						           :userId="null" :keyword="null" :ids="null"/>
 					</div>
