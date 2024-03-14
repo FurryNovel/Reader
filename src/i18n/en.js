746c4960d02e256028e048d8f66b8ca505a4e7ea
k99k5 <545551320@qq.com>
2024-03-15 01:17:53 +0800

    :earth_americas: refactor(index.vue): add i18n support for UI text elements

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -12,4 +12,24 @@ export default {
     '章节': 'Chapter',
     '小说详情': 'Novel Details',
     '页面未找到': 'Page Not Found',
+    
+    '确定': 'OK',
+    '清除': 'Clear',
+    
+    '标签': 'Tags',
+    '友情链接': 'Links',
+    
+    '热门精选': 'Popular Selections',
+    '查看更多': 'View More',
+    '随便来点': 'Random',
+    '随机': 'Random',
+    '刷新一下': 'Refresh',
+    '最新小说': 'Latest Novels',
+    '种族': 'Race',
+    '虎': 'Tiger',
+    '龙': 'Dragon',
+    '狼': 'Wolf',
+    '熊': 'Bear',
+    '狗': 'Dog',
+    '猫': 'Cat',
 }
