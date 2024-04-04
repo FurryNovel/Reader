2ebd7e5dbfed2edd13c954227f91a436d6da70f2
k99k5 <545551320@qq.com>
2024-04-05 00:50:39 +0800

    :bug: fix(server): handle 404 status dynamically based on HTML content

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -3,7 +3,7 @@
 		<div class="flex flex-1 flex-col rounded bg-white text-black sm:p-10 dark:bg-surface-600 dark:text-white">
 			<div class="flex flex-col items-center justify-center min-h-[calc(100vh-208px)] max-sm:h-full w-full">
 				<div class="flex flex-col items-center justify-center h-[400px]">
-					<div class="mb-2 text-2xl font-bold">{{ t('页面未找到') }}</div>
+					<div class="mb-2 text-2xl font-bold">404 - {{ t('页面未找到') }}</div>
 					<div class="justify-center text-gray-500 dark:text-white text-center">
 						<p>
 							{{ t('我们可能遇到了一些问题，无法找到这个页面。') }}
